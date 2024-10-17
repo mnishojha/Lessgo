707a07a849ebcf2557f77f0f36699bd7de89c4eb
Manish Ojha <120119872+mnishojha@users.noreply.github.com>
2024-10-18 02:38:14 +0530

    Initial Commit

---
+++ Lessgo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lessgo
+//
+//  Created by manish ojha on 18/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lessgo/LessgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessgoApp.swift
+//  Lessgo
+//
+//  Created by manish ojha on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LessgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
