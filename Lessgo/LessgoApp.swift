a3f8a186926391ffa62601bc408d103569a294ae
Engulfedleader2 <engulfedleader@gmail.com>
2024-11-01 10:40:49 -0400

    Commented out firebase initlaize

--- Lessgo/LessgoApp.swift
+++ Lessgo/LessgoApp.swift
@@ -11,9 +11,9 @@ import Firebase
 @main
 struct LessgoApp: App {
     //Initalize Firebase
-    init(){
-        FirebaseApp.configure()
-    }
+//    init(){
+//        FirebaseApp.configure()
+//    }
     var body: some Scene {
         WindowGroup {
             TravelHomeView()
